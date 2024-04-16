1d354c74ce04045f197973ee44f36ad4a46971bd
Lova Ramih <lova.ramiharisoa@gmail.com>
2024-04-16 15:12:32 +0300

    ajout du fichier EtudiantController.php et configuration des route pour l'Etudiant dans le fichier api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EtudiantController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//créon notre route api
+Route::get("etudiant", [EtudiantController::class, 'listEtudiants']);
+Route::get("etudiant/{id}", [EtudiantController::class, 'getEtudiant']);
+Route::post("creer-etudiant", [EtudiantController::class, 'create']);
+Route::put("update/{id}", [EtudiantController::class, 'update']);
+Route::delete("delete/{id}", [EtudiantController::class, 'delete']);
